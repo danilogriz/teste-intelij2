4696dcb667325b14a568853af7dc4d3b17c5e31b
Danilo Grizante Ventura <danilogrizventura@gmail.com>
2022-03-03 19:57:03 -0300

    Retirado o comentário sobre o hello world

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,9 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
-        /*int a = 5;
+        int a = 5;
         int b = 3;
-        System.out.println("E ai mundao!" + (a+b));*/
+        System.out.println("E ai mundao!" + (a+b));
     }
 
 }
